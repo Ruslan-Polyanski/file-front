5768ff7fb817bbf445a1143988ca5ff204882682
Ruslan-Palyanski <ruslapolyanski@yandex.ru>
2024-10-23 22:14:36 +0300

    feat: add base url

--- src/app/router/Router.tsx
+++ src/app/router/Router.tsx
@@ -6,30 +6,33 @@ import { Profile } from '../../pages/profile/Profile';
 import { Employees } from '../../pages/employees/Employees';
 import { Car } from '../../pages/car/Car';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    errorElement: <Error />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: 'employees',
-        element: <Employees />,
-      },
-      {
-        path: 'car',
-        element: <Car />,
-      },
-      {
-        path: 'profile',
-        element: <Profile />,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Layout />,
+      errorElement: <Error />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: 'employees',
+          element: <Employees />,
+        },
+        {
+          path: 'car',
+          element: <Car />,
+        },
+        {
+          path: 'profile',
+          element: <Profile />,
+        },
+      ],
+    },
+  ],
+  { basename: '/file-front' },
+);
 
 export { router };
