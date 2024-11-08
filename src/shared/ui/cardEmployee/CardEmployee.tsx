b808755ac7b639b67452d84a40620cee6650d576
Ruslan-Palyanski <ruslapolyanski@yandex.ru>
2024-11-08 15:12:40 +0300

    feat: add state show timeBox

--- src/shared/ui/cardEmployee/CardEmployee.tsx
+++ src/shared/ui/cardEmployee/CardEmployee.tsx
@@ -31,8 +31,17 @@ const CardEmployee: FC<IEmployee> = ({
   const [breakDate, setBreakDate] = useState<Date | null>(
     setHours(setMinutes(new Date(), 45), 0),
   );
+  const [hideBoxTimeSetters, setРideBoxTimeSetters] = useState<boolean>(false);
 
   const altText = `${firstName} ${lastName} ${surname}`;
+
+  function handleFormControlLabel(
+    event: React.SyntheticEvent,
+    checked: boolean,
+  ) {
+    setРideBoxTimeSetters(checked);
+  }
+
   return (
     <section className={style.employee__box}>
       <div className={style.imageBox}>
@@ -49,59 +58,62 @@ const CardEmployee: FC<IEmployee> = ({
             <FormGroup>
               <FormControlLabel
                 control={<Checkbox />}
+                onChange={handleFormControlLabel}
                 label={<span style={{ fontSize: '16px' }}>{'Прогул'}</span>}
               />
             </FormGroup>
           </div>
-          <div className={style.boxTimeSetters}>
-            <div className={style.timeSetter}>
-              <span>Начало</span>
-              <div>
-                <DatePicker
-                  selected={startDate}
-                  onChange={(date) => setStartDate(date)}
-                  showTimeSelect
-                  showTimeSelectOnly
-                  timeIntervals={15}
-                  dateFormat="HH:mm"
-                  showTimeCaption={false}
-                  locale="ru"
-                />
+          {hideBoxTimeSetters ? null : (
+            <div className={style.boxTimeSetters}>
+              <div className={style.timeSetter}>
+                <span>Начало</span>
+                <div>
+                  <DatePicker
+                    selected={startDate}
+                    onChange={(date) => setStartDate(date)}
+                    showTimeSelect
+                    showTimeSelectOnly
+                    timeIntervals={15}
+                    dateFormat="HH:mm"
+                    showTimeCaption={false}
+                    locale="ru"
+                  />
+                </div>
+              </div>
+              <div className={style.timeSetter}>
+                <span>Конец</span>
+                <div>
+                  <DatePicker
+                    selected={endDate}
+                    onChange={(date) => setEndDate(date)}
+                    showTimeSelect
+                    showTimeSelectOnly
+                    timeIntervals={15}
+                    dateFormat="HH:mm"
+                    showTimeCaption={false}
+                    locale="ru"
+                  />
+                </div>
+              </div>
+              <div className={style.timeSetter}>
+                <span>Перерыв</span>
+                <div>
+                  <DatePicker
+                    selected={breakDate}
+                    onChange={(date) => setBreakDate(date)}
+                    showTimeSelect
+                    showTimeSelectOnly
+                    timeIntervals={15}
+                    dateFormat="HH:mm"
+                    minTime={setHours(setMinutes(new Date(), 0), 0)}
+                    maxTime={setHours(setMinutes(new Date(), 0), 4)}
+                    showTimeCaption={false}
+                    locale="ru"
+                  />
+                </div>
               </div>
             </div>
-            <div className={style.timeSetter}>
-              <span>Конец</span>
-              <div>
-                <DatePicker
-                  selected={endDate}
-                  onChange={(date) => setEndDate(date)}
-                  showTimeSelect
-                  showTimeSelectOnly
-                  timeIntervals={15}
-                  dateFormat="HH:mm"
-                  showTimeCaption={false}
-                  locale="ru"
-                />
-              </div>
-            </div>
-            <div className={style.timeSetter}>
-              <span>Перерыв</span>
-              <div>
-                <DatePicker
-                  selected={breakDate}
-                  onChange={(date) => setBreakDate(date)}
-                  showTimeSelect
-                  showTimeSelectOnly
-                  timeIntervals={15}
-                  dateFormat="HH:mm"
-                  minTime={setHours(setMinutes(new Date(), 0), 0)}
-                  maxTime={setHours(setMinutes(new Date(), 0), 4)}
-                  showTimeCaption={false}
-                  locale="ru"
-                />
-              </div>
-            </div>
-          </div>
+          )}
         </div>
       </div>
     </section>
