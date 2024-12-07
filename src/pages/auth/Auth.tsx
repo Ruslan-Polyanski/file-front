92157c2b0237a816714066f4869f1b02b1ff2e8f
Ruslan-Palyanski <ruslapolyanski@yandex.ru>
2024-12-07 23:57:47 +0300

    feat: add auth page with logic

--- src/pages/auth/Auth.tsx
+++ src/pages/auth/Auth.tsx
@@ -1,7 +1,76 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
+
+const URL = 'http://localhost:3001/api/auth/login';
+
+interface IUser {
+  email: string;
+  password: string;
+}
+
+interface IUserResponse {
+  id: number;
+  email: string;
+  token: string;
+}
 
 const Auth: FC = () => {
-  return <div></div>;
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [userDataResponse, setUserDataResponse] =
+    useState<IUserResponse | null>(null);
+  // const [isLogin, setIsLogin] = useState(false);
+  // const [error, setError] = useState('');
+
+  const handleChangeEmail: React.ChangeEventHandler<HTMLInputElement> = (
+    event,
+  ) => {
+    setEmail(event.target.value);
+  };
+
+  const handleChangePassword: React.ChangeEventHandler<HTMLInputElement> = (
+    event,
+  ) => {
+    setPassword(event.target.value);
+  };
+
+  const handleClickButtonSendForm: React.MouseEventHandler<
+    HTMLButtonElement
+  > = async (event) => {
+    event.preventDefault();
+    const user: IUser = {
+      email,
+      password,
+    };
+
+    fetch(URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setUserDataResponse(data);
+      })
+      .catch(() => {
+        console.log('ConsoleError');
+      });
+  };
+
+  return (
+    <div>
+      <form>
+        <input value={email} type="text" onChange={handleChangeEmail} />
+        <input
+          value={password}
+          type="password"
+          onChange={handleChangePassword}
+        />
+        <button onClick={handleClickButtonSendForm}>Send</button>
+      </form>
+    </div>
+  );
 };
 
 export { Auth };
