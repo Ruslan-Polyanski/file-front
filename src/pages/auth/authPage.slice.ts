f81f7918906e0976e07e9f915207efccd80cb01e
Ruslan-Palyanski <ruslapolyanski@yandex.ru>
2024-12-28 17:24:13 +0300

    refactor: change base url

--- src/pages/auth/authPage.slice.ts
+++ src/pages/auth/authPage.slice.ts
@@ -40,7 +40,7 @@ export const { setIsAuth, setIsLoader } = authSlice.actions;
 export const createLogin = createAsyncThunk(
   '@@Auth/createLogin',
   async (user: IUser, { dispatch }) => {
-    fetch('http://localhost:3001/api/auth/login', {
+    fetch('/api/auth/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -70,7 +70,7 @@ export const createCheckAuth = createAsyncThunk(
     const access_token = LocalStorage.getItem('token');
 
     if (access_token) {
-      fetch('http://localhost:3001/api/auth/profile', {
+      fetch('/api/auth/profile', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
