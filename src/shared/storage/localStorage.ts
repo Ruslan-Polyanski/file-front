0a367d34b487103f8e9a96502ea8fe049337a67d
Ruslan-Palyanski <ruslapolyanski@yandex.ru>
2025-03-01 21:27:48 +0300

    feat: add class for work with localstorage

--- src/shared/storage/localStorage.ts
+++ src/shared/storage/localStorage.ts
@@ -1,27 +1,35 @@
-const LocalStorage = {
-  getItem: (key: string): string | null => {
+class LocalStorage {
+  private readonly key: string;
+
+  constructor(key: string) {
+    this.key = key;
+  }
+
+  get(): string | null {
     let result: string | null;
     try {
-      const value = localStorage.getItem(key) ?? '';
+      const value = localStorage.getItem(this.key) ?? '';
       result = JSON.parse(value);
     } catch (err) {
       result = null;
     }
     return result;
-  },
+  }
 
-  setItem: <T>(key: string, value: T) => {
-    const data: string = JSON.stringify(value);
-    localStorage.setItem(key, data);
-  },
+  set<T>(value: T) {
+    localStorage.setItem(this.key, JSON.stringify(value));
+  }
 
-  deleteItem: (key: string) => {
-    localStorage.removeItem(key);
-  },
+  delete() {
+    localStorage.removeItem(this.key);
+  }
 
-  deleteAll: () => {
+  static clear() {
     localStorage.clear();
-  },
-};
+  }
+}
 
-export { LocalStorage };
+export const access_token = new LocalStorage('access_token')
+export const refresh_token = new LocalStorage('refresh_token')
+export { LocalStorage }
+
