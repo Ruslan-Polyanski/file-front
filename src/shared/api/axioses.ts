10e9dedb78da3ed2e21fd2558fb9d53a859cacb3
Ruslan-Palyanski <ruslapolyanski@yandex.ru>
2025-03-22 22:44:32 +0300

    feat: change api module

--- src/shared/api/axioses.ts
+++ src/shared/api/axioses.ts
@@ -16,39 +16,43 @@ class API {
         this.baseURL = baseURL;
     }
 
-    private async fetcher<T, U extends HeadersInit, K>(path: T, method: string, headers: U, body?: K): Promise<any> {
-        const response = await fetch(this.baseURL + path, {
-            method,
-            headers,
-            ...(body && {body: JSON.stringify(body)})
-            });
-            
-            if (!response.ok) {
-                const error: { error: string, message: string, statusCode: number} = await response.json();
-                if(error.error && error.message && error.statusCode) {
-                    throw new Error(`Error ${error.statusCode}: ${error.error} - ${error.message}`)
+    private async fetcher<T, U extends HeadersInit, K, S extends AbortSignal>(path: T, method: string, headers: U, body?: K, signal?: S): Promise<any> {
+            const response = await fetch(this.baseURL + path, {
+                method,
+                headers,
+                ...(body && {body: JSON.stringify(body)}),
+                signal
+                });
+                
+                if (!response.ok) {
+                    const error: { error: string, message: string, statusCode: number} = await response.json();
+
+                    const castomError = {
+                        status: error.statusCode || response.status,
+                        reason: response.statusText,
+                        message: error.message === 'Unauthorized' ? response.url : error.message
+                    }
+                    
+                    throw castomError
                 }
-
-                throw new Error(`Error ${response.status}: ${response.statusText}`)
-            }
-
-        return await response.json();
+    
+            return await response.json();
     }
 
-    public get<T extends string, U extends Record<string, string>>(path: T, headers: U) {
-        return this.fetcher(path, METHOD.GET, headers)
+    public get<T extends string, U extends Record<string, string>, S extends AbortSignal>(path: T, headers: U, signal?: S) {
+        return this.fetcher(path, METHOD.GET, headers, signal)
     }
 
-    public post<T extends string, U extends Record<string, string>, K extends Record<string, unknown>>(path: T, headers: U, body: K) {
-        return this.fetcher(path, METHOD.POST, headers, body)
+    public post<T extends string, U extends Record<string, string>, K extends Record<string, unknown>, S extends AbortSignal>(path: T, headers: U, body: K, signal?: S) {
+        return this.fetcher(path, METHOD.POST, headers, body, signal)
     }
 
-    public put<T extends string, U extends Record<string, string>, K extends Record<string, unknown>>(path: T, headers: U, body: K) {
-        return this.fetcher(path, METHOD.PUT, headers, body)
+    public put<T extends string, U extends Record<string, string>, K extends Record<string, unknown>, S extends AbortSignal>(path: T, headers: U, body: K, signal?: S) {
+        return this.fetcher(path, METHOD.PUT, headers, body, signal)
     }
 
-    public patch<T extends string, U extends Record<string, string>, K extends Record<string, unknown>>(path: T, headers: U, body: K) {
-        return this.fetcher(path, METHOD.PATCH, headers, body)
+    public patch<T extends string, U extends Record<string, string>, K extends Record<string, unknown>, S extends AbortSignal>(path: T, headers: U, body: K, signal?: S) {
+        return this.fetcher(path, METHOD.PATCH, headers, body, signal)
     }
 }
 
